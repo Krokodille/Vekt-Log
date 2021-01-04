291efe6faad39a53feb0496df4c4396c2b7c766d
Patrick <patrickingeberg.pi@gmail.com>
2021-01-04 02:22:10 +0100

    added image support for users
    Added so that users can upload a profil picture.

--- dbschema.js
+++ dbschema.js
@@ -1,9 +1,29 @@
 let db = {
-	weights: [
+	users: [
+		{
+			uderId: "dh23ggj6h32g543j5gf43",
+			userHandle: "user",
+			weightNr: 199,
+			createdAt: "2020-12-30T23:15:19.713Z",
+			imageUrl: "image/udghnfbubgdf/duishb",
+		},
+	],
+	weight: [
 		{
 			userHandle: "user",
 			weightNr: 199,
 			createdAt: "2020-12-30T23:15:19.713Z",
 		},
 	],
+};
+
+const userDetails = {
+	// Redux data
+	credentials: {
+		userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+		email: "user@email.com",
+		handle: "user",
+		createdAt: "2019-03-15T10:59:52.798Z",
+		imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+	},
 };
